55de06ab78c5f600cb829519aa0a2de3abe53833
Liudmyla Popova <liudmyla@Liudmylas-MacBook-Pro-2.local>
2026-04-02 18:24:24 +0200

    test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,10 @@
+import { timeout } from "rxjs";
+
 describe('AppController (e2e)', () => {
   it('/ (GET)', async () => {
     const res = await fetch('http:/localhost:3000/');
     expect(res.status).toBe(200);
     const message = await res.json();
     expect(message).toEqual({ msg: 'Ecommerce Order Platform' });
-  });
+  }, 60*1000);
 });
